4b46a71acf4df3946198e37849424d61f758a100
Ouaghrem Issam <OUAGHREM.issam@esprit.tn>
2026-05-21 23:08:10 +0100

    Expositons des API + Swagger en openapi.json

--- Gestion_UE_Etudiant/src/main/java/webservices/ApplicationMain.java
+++ Gestion_UE_Etudiant/src/main/java/webservices/ApplicationMain.java
@@ -1,8 +1,18 @@
 package webservices;
 
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.Set;
 
 @ApplicationPath("api")
 public class ApplicationMain extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Set.of(
+                webservices.UeWs.class,
+                OpenApiResource.class
+        );
+    }
 }
